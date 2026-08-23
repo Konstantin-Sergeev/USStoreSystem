678277f6cb12920e5a484b046b941a9ede33b086
Konstantin-Sergeev <email@example.com>
2026-08-24 02:20:54 +0300

    stores page finished + fixes

--- usss/store_service/forms.py
+++ usss/store_service/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class StoreForm(forms.Form):
-    store_id = forms.CharField(required=False)
-    state = forms.CharField(required=True)
-    market = forms.CharField(required=True)
-    market_size = forms.CharField(required=True)
+    store_id = forms.CharField(required=True)
+    state = forms.CharField(required=False)
+    market = forms.CharField(required=False)
+    market_size = forms.CharField(required=False)
     store_code = forms.IntegerField(required=True)
